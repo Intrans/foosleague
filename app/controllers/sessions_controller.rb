9f057e4fcab10a8f7ea36ee528ad1656d65f716c
Sean McCann <sean@seanmccann.ca>
2012-10-12 19:52:04 -0600

    dont specify layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,9 @@
 class SessionsController < Devise::SessionsController
   respond_to :html
-  
+
   skip_before_filter :authorized?
   skip_before_filter :authenticate_player!
 
-  layout :specify_layout
-
   prepend_view_path 'app/views/devise'
-
-  protected
-    def specify_layout
-      controller_name == 'sessions' && (action_name == 'new' || action_name == 'create')  ? 'layouts/auth' : 'layouts/application'
-    end
 
 end
