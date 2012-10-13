1854c5dbd2dff81fd1916fd6aa03722964d6ee85
Sean McCann <sean@seanmccann.ca>
2012-10-13 04:36:16 -0600

    clean up create_resource for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,18 +9,16 @@ class PlayersController < InheritedResources::Base
   private
 
     def create_resource(object)
-      # Check if the user already exists.
+      # Check if the player already exists.
       @player = Player.find_by_twitter_name(resource_params[0][:twitter_name].downcase)
-      if @player.present?
-        membership = @league.league_memberships.create(:player => @player)
-        return true if membership.valid?
-        @player = object
-        return false
+      unless @player.present?
+        @player = Player.create!(:twitter_name => resource_params[0][:twitter_name].downcase) if object.valid?
       end
-
-      # # Invite the user if they do not.
-      @player = Player.create!(:twitter_name => resource_params[0][:twitter_name].downcase) if object.valid?
-      membership = @league.league_memberships.create(:player => @player) if @player.valid?
-      @player.valid?
+      
+      membership = parent.league_memberships.create do |league_membership|
+        league_membership.player = @player
+      end if @player.valid?
+      
+      membership.valid?
     end
 end
