3be851eadb91799aef579f78d48f59fc58f32a69
Sean McCann <sean@seanmccann.ca>
2012-10-12 18:33:11 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foosleague::Application.config.secret_token = '411a3f5dd0fb828ba9e88b0b251171703ae6c45a3e804855a792dda8553f86e62678eb560dd177b96fb2f8cd85c3e80b25a3a0fbfa744af7f8a572b306ea42b6'
