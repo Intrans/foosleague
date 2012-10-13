bc2bed3ce107c24dc4371ebefc9c4e703ef53776
Michael Deering <mdeering@mdeering.com>
2012-10-13 00:26:40 -0600

    blahas

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,7 @@
 class League < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :league_memberships
+  has_many :players, :through => :league_memberships
+
 end
--- app/models/league_membership.rb
+++ app/models/league_membership.rb
@@ -1,3 +1,5 @@
 class LeagueMembership < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :league
+  belongs_to :player
 end
