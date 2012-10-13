aeb8646fc1930dc7afcb0723814021fbcb3812b2
Sean McCann <sean@seanmccann.ca>
2012-10-13 04:20:53 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
 ["Freedom Freight", "Mitre Media", "Lift Interactive"].each do |league|
-  League.create! :name => league
+  League.create!(:name => league)
 end
 
-Player.create! :twitter_name => "sea", :name => "Sean McCann"
-Player.create! :twitter_name => "mdeering", :name => "Michael Deering"
-Player.create! :twitter_name => "agyuricska", :name => "Adrian Gyuricska"
-Player.create! :twitter_name => "nickvisual", :name => "Andrew Nickerson"
+Player.create!(:twitter_name => "sea", :name => "Sean McCann")
+Player.create!(:twitter_name => "mdeering", :name => "Michael Deering")
+Player.create!(:twitter_name => "agyuricska", :name => "Adrian Gyuricska")
+Player.create!(:twitter_name => "nickvisual", :name => "Andrew Nickerson")
 
 League.all.each do |league|
   Player.all.each do |player|
-    league.league_memberships.create do |league_membership|
-      league_membership.player = player
-    end
+    league.players << player
   end
 end
