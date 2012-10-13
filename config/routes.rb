8184b03afecbd2d4e00b2da0228d457caffe0081
Michael Deering <mdeering@mdeering.com>
2012-10-12 23:28:11 -0600

    leagues, teams and other random shit.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Foosleague::Application.routes.draw do
   devise_for :players, :path_names => {sign_in: "login", sign_out: "logout"},
                        :controllers => {:sessions => "sessions", :registrations => 'registrations', :passwords => 'passwords', :omniauth_callbacks => "omniauth_callbacks"}
 
+  authenticated :player do
+    root :to => "leagues#index"
+  end
+
   root :to => Stationary::Engine
   match '/:path(.:format)', :to => Stationary::Engine, :constraints => { :path => /.+?/ }
 end
