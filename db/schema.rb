92836cf63af09182530738a533155697f6a5de97
Sean McCann <sean@seanmccann.ca>
2012-10-12 23:06:05 -0600

    sort out migrations

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,17 @@ ActiveRecord::Schema.define(:version => 20121013010628) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "authentication_token"
-    t.string   "name"
-    t.string   "twitter_name"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "twitter_name"
   end
 
   add_index "players", ["authentication_token"], :name => "index_players_on_authentication_token", :unique => true
   add_index "players", ["email"], :name => "index_players_on_email", :unique => true
   add_index "players", ["reset_password_token"], :name => "index_players_on_reset_password_token", :unique => true
+  add_index "players", ["twitter_name"], :name => "index_players_on_twitter_name", :unique => true
 
 end
